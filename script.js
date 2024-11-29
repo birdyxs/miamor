472a620a99514fb579911d28223163ce64d4618a
mar <105406641+birdyxs@users.noreply.github.com>
2024-11-28 23:09:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const audio = document.getElementById('audio');
+const playBtn = document.getElementById('play');
+const pauseBtn = document.getElementById('pause');
+
+playBtn.addEventListener('click', () => {
+  audio.play();
+});
+
+pauseBtn.addEventListener('click', () => {
+  audio.pause();
+});
